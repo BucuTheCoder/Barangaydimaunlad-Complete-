fccb78d7c7ec5253d0370a1262c3e4c61e48e0c8
BucuTheCoder <72145808+BucuTheCoder@users.noreply.github.com>
2021-05-24 19:43:37 +0600

    Delete BarangayDi-Maunlad directory

--- BarangayDi-Maunlad/Database/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-	session_start();
-	
-	$_SESSION['loggedIn'] = 'false';
-	unset($_SESSION['access']);
-	unset($_SESSION['username']);
-	header("location: /login.php"); 
-?>
--- BarangayDi-Maunlad/Database/permission.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-if($_SESSION['access'] == 'regular'){
-  echo "<script> alert('You do not have permission to access this site'); window.location='index.php'; </script>";  
- }
-
-
-?>
